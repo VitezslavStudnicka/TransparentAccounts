735c550f3bb91815fbd8995c3db5488fde155d63
Vítězslav Studnička <vitezslav.studnicka@gmail.com>
2020-02-12 00:39:22 +0100

    Transaction Detail

--- app/src/main/java/com/vs/sample/transparentaccounts/fragments/FragmentAccountDetail.kt
+++ app/src/main/java/com/vs/sample/transparentaccounts/fragments/FragmentAccountDetail.kt
@@ -1,34 +1,24 @@
 package com.vs.sample.transparentaccounts.fragments
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.vs.sample.transparentaccounts.R
-import com.vs.sample.transparentaccounts.viewmodels.FragmentAccountDetailVM
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.vs.sample.transparentaccounts.databinding.FragmentAccountDetailBinding
 
 class FragmentAccountDetail : Fragment() {
 
-    companion object {
-        fun newInstance() = FragmentAccountDetail()
-    }
 
-    private lateinit var viewModel: FragmentAccountDetailVM
+    val args: FragmentAccountDetailArgs by navArgs()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_account_detail, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentAccountDetailBinding.inflate(inflater, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(FragmentAccountDetailVM::class.java)
-        // TODO: Use the ViewModel
+        binding.account = args.account
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/vs/sample/transparentaccounts/viewmodels/FragmentAccountDetailVM.kt
+++
@@ -1,7 +0,0 @@
-package com.vs.sample.transparentaccounts.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class FragmentAccountDetailVM : ViewModel() {
-    // TODO: Implement the ViewModel
-}
