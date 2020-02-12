d4a83f7de3164cf7b473cd71fa09e464fcf461bd
Vítězslav Studnička <vitezslav.studnicka@gmail.com>
2020-02-12 20:52:08 +0100

    Hidden apikey request interceptor

--- app/src/main/java/com/vs/sample/transparentaccounts/repository/Rest.kt
+++ app/src/main/java/com/vs/sample/transparentaccounts/repository/Rest.kt
@@ -1,7 +1,9 @@
 package com.vs.sample.transparentaccounts.repository
 
 import com.google.gson.GsonBuilder
+import com.vs.sample.transparentaccounts.BuildConfig
 import com.vs.sample.transparentaccounts.utils.Consts
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -11,9 +13,18 @@ class Rest {
 
     private val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
 
+    private val apiKeyInterceptor = Interceptor { chain ->
+        val header = chain.request().headers.newBuilder().add("web-api-key", BuildConfig.TransparentApiKey).build()
+        val request = chain.request()
+            .newBuilder()
+            .headers(header)
+            .build()
+        chain.proceed(request)
+    }
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(Consts.URL_MOCK_2)
-        .client(OkHttpClient.Builder().addInterceptor(interceptor).build())
+        .client(OkHttpClient.Builder().addInterceptor(apiKeyInterceptor).addInterceptor(interceptor).build())
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setDateFormat(Consts.FORMAT_DATE_TIME_REST).create()))
         .build()
 
